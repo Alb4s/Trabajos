0067a15a48a91e85fc2669d9324fe02b2257e78c
Alb4s <enzoimfeld10@gmail.com>
2026-05-21 05:25:56 -0300

    Finished

--- 6ejercicios_ordenamientos/Ordenamientos.py
+++ 6ejercicios_ordenamientos/Ordenamientos.py
@@ -1,14 +1,12 @@
 from colorama import Fore
-# Ejercicio 1: Dadas las siguientes listas:
-# Nombres =
-# ["Ana","Luis","Juan","Sol","Roberto","Sonia","Ulises","Sofia","Maria","Pedro","Anto
-# nio", "Eugenia", "Soledad", "Mario", "Mariela"]
-# Edades = [23,45,34,23,46,23,45,67,37,68,25,55,45,27,43]
-# Desarrollar una función que realice el ordenamiento de las listas por nombre de
-# manera ascendente.
+import sys
 
-from funciones import ordenar_listas, ordenar_listas_2, ordenar_listas_3
+from funciones import ordenar_listas, ordenar_listas_2, ordenar_listas_3, ordenar_mexico, ordenar_menor, postal_zip
 from listas import Nombres, Edades, Materias, Puntos, Estudiantes, Apellidos, Nota
+sys.path.append('4Funciones_Listas')
+from Listas_pais import nombres_pais, telefonos,  mails, address, postalZip, region, country, edades_pais
+
+
 print(f"{Fore.GREEN}---------------------------------------Ejercicio 1---------------------------------------{Fore.RESET}")
 ejercicio1 = ordenar_listas(Nombres,Edades)
 for par in ejercicio1:
@@ -16,14 +14,6 @@ for par in ejercicio1:
 
 print()
 
-# Ejercicio 2: Dadas las siguientes listas:
-# Nombres = ["Matematica","Investigacion Operativa","Ingles","Literatura","Ciencias
-# Sociales","Computacion","Ingles","Algebra","Contabilidad","Artistica", "Algoritmos",
-# "Base de Datos", "Ergonomia", "Naturaleza"]
-# Puntos = [100,98,56,25,87,38,64,42,28,91,66,35,49,57,98]
-# Desarrollar una función que realice el ordenamiento de las listas por nombre de
-# manera ascendente, si el nombre es el mismo, debe ordenar por puntos de manera
-# descendente.
 
 print(f"{Fore.GREEN}---------------------------------------Ejercicio 2---------------------------------------{Fore.RESET}")
 ejercicio2 = ordenar_listas_2(Materias, Puntos)
@@ -32,24 +22,32 @@ for orden in ejercicio2:
 
 print()
 
-# Ejercicio 3: Dadas las siguientes listas:
-# Estudiantes =
-# ["Ana","Luis","Juan","Sol","Roberto","Sonia","María","Sofia","Maria","Pedro","Anto
-# nio", "Eugenia", "Soledad", "Mario", "María"]
-# Apellidos =
-# [“Sosa”,”Gutierrez”,”Alsina”,”Martinez”,”Sosa”,”Ramirez”,”Perez”,”Lopez”,”Arregui”
-# ,”Mitre”,”Andrade”,”Loza”,”Antares”,”Roca”,”Perez”]
-# Nota = [8,4,9,10,8,6,4,8,7,5,6,7,10,4,8]
-# Desarrollar una función que realice el ordenamiento de las listas por apellido de
-# manera ascendente, si el apellido es el mismo, debe ordenar por nombre de manera
-# ascendente, si el nombre también es el mismo, debe ordenar por nota de manera
-# descendente.
 
 print(f"{Fore.GREEN}---------------------------------------Ejercicio 3---------------------------------------{Fore.RESET}")
 ejercicio3 = ordenar_listas_3(Estudiantes, Apellidos, Nota)
 for asignatura in ejercicio3:
     print(asignatura)
 
+print()
+
 # Ejercicio 4: Una startup desea analizar las estadísticas de los usuarios de su sitio de
 # compras on-line que recientemente han lanzado al mercado, para ello necesita un programa
 # que le permita acceder a los datos relevados.
+print(f"{Fore.GREEN}---------------------------------------Ejercicio 4---------------------------------------{Fore.RESET}")
+ejercicio4 = ordenar_mexico(nombres_pais, edades_pais, mails, telefonos, country)
+for mexico in ejercicio4:
+    print(mexico)
+
+print()
+
+ejercicio4_2 = ordenar_menor(edades_pais, nombres_pais)
+for menor in ejercicio4_2:
+    print(menor)
+
+print()
+
+ejercicio4_3 = postal_zip(nombres_pais, edades_pais, country, postalZip)
+for postal in ejercicio4_3:
+    print(postal)
+
+#DESARROLLÉ TODO LO QUE FALTA EN LA RECIÉN ACTUALIZADA "4Funciones_Listas"
--- 6ejercicios_ordenamientos/funciones.py
+++ 6ejercicios_ordenamientos/funciones.py
@@ -1,22 +1,52 @@
 def ordenar_listas(name, age):
     pares = list(zip(name, age))
-    
     ordenamiento = sorted(pares, key=lambda x: x[0])
 
     return ordenamiento
 
 def ordenar_listas_2(assignature, points):
-
     orden = list(zip(assignature, points))
-
     materias = sorted(orden, key=lambda x: (x[0], -x[1]) )
     
     return materias
 
 def ordenar_listas_3(students, lastname, note):
-    
     nombre_completo_nota = list(zip(students,lastname,note))
-
     ordenar = sorted(nombre_completo_nota, key=lambda x: (x[0], x[1], -x[2]))
 
     return ordenar
+
+def ordenar_mexico(name, age, phone, mail, count):
+    lista = []
+    mexico = list(zip(name, age, phone, mail, count))
+    for i in mexico:
+        if i[4] == "Mexico":
+            lista.append(i)
+    ordenar = sorted(lista, key=lambda x: (x[0]))
+
+    return ordenar
+
+
+def ordenar_menor(age, name):
+    lista = []
+    menor_lista = list(zip(age, name))
+    minimo = min(age)
+
+    for i in menor_lista:
+        if i[0] == minimo:
+            lista.append(i)
+    
+    ordenar = sorted(lista, key=lambda x: (x[0], x[1]))
+
+    return ordenar
+
+def postal_zip(name, age, country, pz):
+    lista = []
+    codigo_lista = list(zip(name, age, country, pz))
+
+    for i in codigo_lista:
+        if (i[2] == "Mexico" or i[2] == "Brazil") and i[3] > 8000:
+            lista.append(i)
+    ordenar_codigos = sorted(lista, reverse=True, key=lambda x: (x[0], x[1]))
+
+    return ordenar_codigos

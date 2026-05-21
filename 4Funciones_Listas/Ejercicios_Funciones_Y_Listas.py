ed1c0414becbbcbe92ce70781f6284956d520926
Alb4s <enzoimfeld10@gmail.com>
2026-05-21 05:24:19 -0300

    Actualizado para Ejercicio N°6

--- 4Funciones_Listas/Ejercicios_Funciones_Y_Listas.py
+++ 4Funciones_Listas/Ejercicios_Funciones_Y_Listas.py
@@ -127,7 +127,7 @@ for i in menor:
 # listas_personas.py. Importar los nombres a una lista. Realizar una función que
 # muestre los mismos.
 
-from Listas import nombres
+from Listas_pais import nombres_pais
 
 def mostrar_nombres(lista):
     for nombre in lista:
@@ -151,8 +151,10 @@ mostrar_nombres(nombres)
 # sea mayor a 8000
 # 8-Listar nombre, mail y teléfono de los usuarios italianos mayores a 40
 # años.
-
-from Listas import nombres, telefonos,  mails, address, postalZip, region, country, edades
+import sys
+sys.path.append('6ejercicios_ordenamientos')
+import funciones
+from Listas_pais import nombres_pais, telefonos,  mails, address, postalZip, region, country, edades_pais
 import Funciones_de_Ejercicios
 
 def menu():
@@ -163,9 +165,10 @@ def menu():
     print("5. Mayor edad de Brasil")
     print("6. México y Brasil con PZ > 8000")
     print("7. Italianos mayores a 40")
-
-# Ejercicio 8: Crear una función para cada opción de menú.
-
+    print("8. Ordenar México")
+    print("9. Ordenar jóvenes")
+    print("10. Ordenar Brasil y México con PZ > 8000")
+# Ejercicio 8: Crear una función para cada opción de menú. AHORA TAMBIÉN PARA ORDENAMIENTO!!!!!!
 
 menu()
 op = input("Elegir opción: ")
@@ -173,31 +176,48 @@ while not op.isdigit():
     op = input("Error.Fuera de parámetro. Reingresar: ")
 op = int(op)
 
-while op < 1 or op > 7:
+while op < 1 or op > 10:
     op = int(input("Error.Fuera de parámetro. Reingresar: "))
 
+print()
 
 if op == 1:
-    Funciones_de_Ejercicios.U_Mexico(nombres, edades, mails, telefonos, country)
+    Funciones_de_Ejercicios.U_Mexico(nombres_pais, edades_pais, mails, telefonos, country)
 
 elif op == 2:
-    Funciones_de_Ejercicios.U_Brasil(nombres, mails, telefonos, country)
+    Funciones_de_Ejercicios.U_Brasil(nombres_pais, mails, telefonos, country)
 
 elif op == 3:
-    Funciones_de_Ejercicios.mas_joven(nombres, edades)
+    Funciones_de_Ejercicios.mas_joven(nombres_pais, edades_pais)
 
 elif op == 4:
-    Funciones_de_Ejercicios.promedio_edades(edades)
+    Funciones_de_Ejercicios.promedio_edades(edades_pais)
 
 elif op == 5:
-    Funciones_de_Ejercicios.mayor_brasil(nombres, edades, country, mails, telefonos, address, postalZip, region)
+    Funciones_de_Ejercicios.mayor_brasil(nombres_pais, edades_pais, country)
 
 elif op == 6:
-    Funciones_de_Ejercicios.mexico_brasil_cp(nombres, country, postalZip)
+    Funciones_de_Ejercicios.mexico_brasil_PZ(nombres_pais, country, postalZip)
 
 elif op == 7:
-    Funciones_de_Ejercicios.italianos_mayores(nombres, mails, telefonos, country, edades)
+    Funciones_de_Ejercicios.italianos_mayores(nombres_pais, mails, telefonos, country, edades_pais)
 
+elif op == 8:
+    resultado = funciones.ordenar_mexico(nombres_pais, edades_pais, mails, telefonos, country)
+    for i in resultado:
+        print(i[0], i[1], i[2], i[3])
+
+elif op == 9:
+    resultado = funciones.ordenar_menor(edades_pais, nombres_pais)
+    for i in resultado:
+        print(i[0], i[1])
+
+elif op == 10:
+    resultado = funciones.postal_zip(nombres_pais, edades_pais, country, postalZip)
+    for i in resultado:
+        print(i[0], i[1], i[2], i[3])
+
+print()
 # Ejercicio 9: Desarrollar las funciones en una biblioteca.
 
 #""""""""hecho"""""""""
--- 4Funciones_Listas/Funciones_de_Ejercicios.py
+++ 4Funciones_Listas/Funciones_de_Ejercicios.py
@@ -1,41 +1,40 @@
-from Listas import nombres, telefonos,  mails, address, postalZip, region, country, edades
-
-def U_Mexico():
-    for i in range(len(nombres)):
+def U_Mexico(name, age, mail, phone, country):
+    for i in range(len(name)):
         if country[i] == "Mexico":
-            print(nombres[i], edades[i], mails[i], telefonos[i])
+            print(name[i], age[i], mail[i], phone[i])
 
-def U_Brasil():
-    for i in range(len(nombres)):
+
+def U_Brasil(name, mail, phone, country):
+    for i in range(len(name)):
         if country[i] == "Brazil":
-            print(nombres[i], mails[i], telefonos[i])
+            print(name[i], mail[i], phone[i])
 
-def mas_joven():
-    menor = min(edades)
-    for i in range(len(edades)):
-        if edades[i] == menor:
-            print(nombres[i], edades[i])
+def mas_joven(name, age):
+    menor = min(age)
+    for i in range(len(age)):
+        if age[i] == menor:
+            print(name[i], age[i])
 
-def promedio_edades():
-    print(sum(edades) / len(edades))
+def promedio_edades(age):
+    print(sum(age) / len(age))
 
-def mayor_brasil():
+def mayor_brasil(name, age, country):
     mayor = -1
     indice = -1
 
-    for i in range(len(edades)):
-        if country[i] == "Brazil" and edades[i] > mayor:
-            mayor = edades[i]
+    for i in range(len(age)):
+        if country[i] == "Brazil" and age[i] > mayor:
+            mayor = age[i]
             indice = i
     
-    print(nombres[indice], edades[indice])
+    print(name[indice], age[indice])
 
-def mexico_brasil_PZ():
-    for i in range(len(nombres)):
-        if (country[i] == "Mexico" or country[i] == "Brazil") and postalZip[i] > 8000:
-            print(nombres[i], country[i], postalZip[i])
+def mexico_brasil_PZ(name, country, pz):
+    for i in range(len(name)):
+        if (country[i] == "Mexico" or country[i] == "Brazil") and pz[i] > 8000:
+            print(name[i], country[i], pz[i])
 
-def italianos_mayores():
-    for i in range(len(nombres)):
-        if country[i] == "Italy" and edades[i] > 40:
-            print(nombres[i], mails[i], telefonos[i])
+def italianos_mayores(name, mail, phone, country, age):
+    for i in range(len(name)):
+        if country[i] == "Italy" and age[i] > 40:
+            print(name[i], mail[i], phone[i])

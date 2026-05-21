59da8c83c8511bd226f89c0a17a956cfb7c069d0
Alb4s <enzoimfeld10@gmail.com>
2026-05-21 01:53:52 -0300

    In Progress

--- 6ejercicios_ordenamientos/listas.py
+++ 6ejercicios_ordenamientos/listas.py
@@ -12,4 +12,3 @@ Estudiantes = ["Ana","Luis","Juan","Sol","Roberto","Sonia","María","Sofia",
     "Maria","Pedro","Antonio", "Eugenia", "Soledad", "Mario", "María"]
 Apellidos = ["Sosa", "Gutierrez","Alsina","Martinez","Sosa","Ramirez","Perez","Lopez","Arregui","Mitre","Andrade","Loza","Antares","Roca","Perez"]
 Nota = [8,4,9,10,8,6,4,8,7,5,6,7,10,4,8]
-
